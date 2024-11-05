ff214dcca672c1f6a7a3e5258a958563c3be5226
LexoBrunett <56777101+LexoBrunett@users.noreply.github.com>
2024-11-05 21:30:06 +0000

    random forest added for diabetes

--- src/app.py
+++ src/app.py
@@ -2,3 +2,53 @@ from utils import db_connect
 engine = db_connect()
 
 # your code here
+import pandas as pd
+
+diabetes_data = pd.read_csv("https://raw.githubusercontent.com/4GeeksAcademy/decision-tree-project-tutorial/main/diabetes.csv")
+diabetes_data = diabetes_data.drop_duplicates().reset_index(drop = True)
+diabetes_data.head()
+
+
+# No normalization of variables is needed for training this model
+
+# Feature selection
+
+from sklearn.feature_selection import SelectKBest
+from sklearn.model_selection import train_test_split
+
+X = diabetes_data.drop("Outcome", axis = 1)
+y = diabetes_data["Outcome"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
+
+selection_model = SelectKBest(k = 7)
+selection_model.fit(X_train, y_train)
+
+selected_columns = X_train.columns[selection_model.get_support()]
+X_train_sel = pd.DataFrame(selection_model.transform(X_train), columns = selected_columns)
+X_test_sel = pd.DataFrame(selection_model.transform(X_test), columns = selected_columns)
+
+X_train_sel["Outcome"] = y_train.values
+X_test_sel["Outcome"] = y_test.values
+
+train_data = X_train_sel
+
+test_data = X_test_sel
+
+X_train = train_data.drop(["Outcome"], axis = 1)
+y_train = train_data["Outcome"]
+X_test = test_data.drop(["Outcome"], axis = 1)
+y_test = test_data["Outcome"]
+
+from sklearn.ensemble import RandomForestClassifier
+
+model = RandomForestClassifier(n_estimators = 60, random_state = 42)
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+y_pred
+
+from sklearn.metrics import accuracy_score
+
+accuracy_score(y_test, y_pred)
+
